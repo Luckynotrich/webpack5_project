466dcf8d0f4e3b0d30b5f596bcb4819cb70b8e09
Luckynotrich <rich.rhaskell@gmail.com>
2021-02-15 14:15:01 -0800

    add css, scss, sass, postcss, hotreloading

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
+import "./style/index.scss";
+
 const elvenShieldRecipe = {
 	leatherStrips: 2,
 	ironIngot: 1,
 	refinedMoonStone: 4,
 };
+const elvenGauntletRecipe = {
+	...elvenShieldRecipe,
+	leather: 1,
+	refinedMoonStone: 4,
+};
 console.log(elvenShieldRecipe);
+console.log(elvenGauntletRecipe);
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const { node, web } = require("webpack");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+var target = "web";
+let mode = "development";
+if (process.env.NODE_ENV === "production") {
+	mode = "production";
+	target = "browserslist";
+}
+
+module.exports = {
+	mode: mode,
+	target: target,
+	module: {
+		rules: [
+			{
+				test: /\.(s[ac]|c)ss$/i,
+				use: [
+					MiniCssExtractPlugin.loader,
+					"css-loader",
+					"postcss-loader",
+					"sass-loader",
+				],
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+				},
+			},
+		],
+	},
+	plugins: [new MiniCssExtractPlugin()],
+	devtool: "source-map",
+	devServer: {
+		contentBase: "./dist",
+		hot: true,
+	},
+};
